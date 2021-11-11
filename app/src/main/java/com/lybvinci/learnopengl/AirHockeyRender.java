0edcc5087bcf5025a0dfe9a8de8cd245c9f6c3cb
liyanbo.monster <liyanbo.monster@bytedance.com>
2021-11-11 11:05:16 +0800

    temp

--- app/src/main/java/com/lybvinci/learnopengl/AirHockeyRender.java
+++ app/src/main/java/com/lybvinci/learnopengl/AirHockeyRender.java
@@ -17,7 +17,7 @@ import javax.microedition.khronos.opengles.GL10;
 
 class AirHockeyRender implements GLSurfaceView.Renderer {
 
-    private static final int POSITION_COMPONENT_COUNT = 2;
+    private static final int POSITION_COMPONENT_COUNT = 4;
     private static final int COLOR_COMPONENT_COUNT = 3;
     private static final int BYTES_PER_FLOAT = 4;
 //    private static final String U_COLOR = "u_Color";
@@ -54,19 +54,20 @@ class AirHockeyRender implements GLSurfaceView.Renderer {
 //                -0.5f, -0.5f,
 //                0.5f, -0.5f,
 //                0.5f, 0.5f,
-                0, 0, 1f, 1f, 1f,
-                -0.5f, -0.8f, 0.7f, 0.7f, 0.7f,
-                0.5f, -0.8f, 0.7f, 0.7f, 0.7f,
-                0.5f, 0.8f, 0.7f, 0.7f, 0.7f,
-                -0.5f, 0.8f, 0.7f, 0.7f, 0.7f,
-                -0.5f, -0.8f, 0.7f, 0.7f, 0.7f,
+                // Triangle Fan
+                0,        0f,   0f,  1.5f,      1f,   1f,   1f,
+                -0.5f, -0.8f,   0f,    1f,    0.7f, 0.7f, 0.7f,
+                0.5f,  -0.8f,   0f,    1f,    0.7f, 0.7f, 0.7f,
+                0.5f,   0.8f,   0f,    2f,    0.7f, 0.7f, 0.7f,
+                -0.5f,  0.8f,   0f,    2f,    0.7f, 0.7f, 0.7f,
+                -0.5f, -0.8f,   0f,    1f,    0.7f, 0.7f, 0.7f,
                 // Line 1
-                -0.5f, 0f, 1f, 0f, 0f,
-                0.5f, 0f, 1f, 0f, 0f,
+                -0.5f, 0f, 0f, 1.5f, 1f, 0f, 0f,
+                 0.5f, 0f, 0f, 1.5f, 1f, 0f, 0f,
 
                 // mallets
-                0f, -0.4f, 0f, 0f, 1f,
-                0f, 0.4f, 1f, 0f, 0f
+                0f, -0.4f, 0f, 1.25f, 0f, 0f, 1f,
+                0f,  0.4f, 0f, 1.75f, 1f, 0f, 0f
         };
         vertexData = ByteBuffer.allocateDirect(tableVerticesWithTriangles.length * BYTES_PER_FLOAT)
                 .order(ByteOrder.nativeOrder())
