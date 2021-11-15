19bcb87279cefa23c14cea44265dcfb1ac7526ab
liyanbo.monster <liyanbo.monster@bytedance.com>
2021-11-15 14:49:24 +0800

    finish air hockey touch

--- app/src/main/java/com/lybvinci/learnopengl/AirHockeyActivity.java
+++ app/src/main/java/com/lybvinci/learnopengl/AirHockeyActivity.java
@@ -6,6 +6,8 @@ import android.content.pm.ConfigurationInfo;
 import android.opengl.GLSurfaceView;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,6 +17,7 @@ public class AirHockeyActivity extends AppCompatActivity {
     private static final String TAG = "lyb_MainActivity";
     private GLSurfaceView mSurfaceView;
     private boolean renderSet = false;
+    private AirHockeyRender ariHockeyRenderer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,8 +31,36 @@ public class AirHockeyActivity extends AppCompatActivity {
         Log.i(TAG, "gl version:" + deviceConfigurationInfo.getGlEsVersion());
         if (supportsEs2) {
             mSurfaceView.setEGLContextClientVersion(2);
-            mSurfaceView.setRenderer(new AirHockeyRender(this));
+            ariHockeyRenderer = new AirHockeyRender(this);
+            mSurfaceView.setRenderer(ariHockeyRenderer);
             renderSet = true;
+            mSurfaceView.setOnTouchListener(new View.OnTouchListener() {
+                @Override
+                public boolean onTouch(View v, MotionEvent event) {
+                    if (null != event) {
+                        final float normalizedX = event.getX() / v.getWidth() *2 -1;
+                        final float normalizedY = -(event.getY() / v.getHeight() *2 - 1);
+                        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                            mSurfaceView.queueEvent(new Runnable() {
+                                @Override
+                                public void run() {
+                                   ariHockeyRenderer.handleTouchPress(normalizedX, normalizedY);
+                                }
+                            });
+                        } else if (event.getAction() == MotionEvent.ACTION_MOVE) {
+                            mSurfaceView.queueEvent(new Runnable() {
+                                @Override
+                                public void run() {
+                                    ariHockeyRenderer.handleTouchDrag(normalizedX, normalizedY);
+                                }
+                            });
+                        }
+                        return true;
+                    } else {
+                        return false;
+                    }
+                }
+            });
         } else {
             Toast.makeText(this, "不支持es2", Toast.LENGTH_SHORT).show();
         }
